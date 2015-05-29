34bc2508b396f2e4d5a39b75c5e27e32e352fe9b
Kate Wood <katewood611@gmail.com>
2015-05-28 21:45:12 -0700

    creates sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 	    @user = User.new(user_params)
 	    if @user.save
 	      redirect_to users_path
+	      session[:user_id] = @user.id.to_s
 	    else
 	      render :new
 	    end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get "users" => "users#index"
   get "users/new" => "users#new" 
   post "users" => "users#create"
+
+  # defines routes for the sessions controller actions
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
