20a08d795f923952ae6b15d5ff441876eb681062
Kate Wood <katewood611@gmail.com>
2015-05-29 15:25:52 -0700

    adds photo column to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529213226) do
+ActiveRecord::Schema.define(version: 20150529222442) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150529213226) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
   add_index "pets", ["user_id"], name: "index_pets_on_user_id"
