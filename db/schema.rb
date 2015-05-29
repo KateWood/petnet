74f140e2e2acb17abc16d1dbfd15d76c89dca384
Kate Wood <katewood611@gmail.com>
2015-05-29 12:30:12 -0700

    adds pets controller and pet model - no pet views yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529043040) do
+ActiveRecord::Schema.define(version: 20150529192700) do
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "pets", ["user_id"], name: "index_pets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
