aa58a087bad307407624b5b6448fc402f174df25
Kate Wood <katewood611@gmail.com>
2015-06-02 15:07:53 -0700

    converts to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150602201345) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20150602201345) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +41,7 @@ ActiveRecord::Schema.define(version: 20150602201345) do
     t.datetime "image_updated_at"
   end
 
-  add_index "pets", ["user_id"], name: "index_pets_on_user_id"
+  add_index "pets", ["user_id"], name: "index_pets_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150602201345) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "posts", ["pet_id"], name: "index_posts_on_pet_id"
+  add_index "posts", ["pet_id"], name: "index_posts_on_pet_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -59,4 +62,8 @@ ActiveRecord::Schema.define(version: 20150602201345) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "pets", "users"
+  add_foreign_key "posts", "pets"
 end
