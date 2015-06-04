6a6c04727ecf650ad0e20802d35dd2c2b837c1e1
Kate Wood <katewood611@gmail.com>
2015-06-03 17:29:20 -0700

    removes photo columns from madels with paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,43 @@
 class PostsController < ApplicationController
+
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+	    params.require(:post).permit(:title, :body)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602201345) do
+ActiveRecord::Schema.define(version: 20150604002611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20150602201345) do
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.string   "photo"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -45,11 +44,14 @@ ActiveRecord::Schema.define(version: 20150602201345) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "photo"
     t.text     "body"
     t.integer  "pet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["pet_id"], name: "index_posts_on_pet_id", using: :btree
