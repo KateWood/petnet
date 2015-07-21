1d2108652f98af66f5fe5750461bbeeef6fcc06f
Kate Wood <katewood611@gmail.com>
2015-07-20 22:47:53 -0400

    validates presence of image for creating user and pet

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates_attachment_presence :image
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	validates :password, presence: true, confirmation: true
