61f7890e8747dcddaf24a4888ca88c9413ca3d77
Kate Wood <katewood611@gmail.com>
2015-07-21 21:10:48 -0400

    adds flash to application correctly formatted with bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,11 @@ class SessionsController < ApplicationController
   		user = User.find_by(email: params[:login][:email])
 	    if user && user.authenticate(params[:login][:password])
 	      	session[:user_id] = user.id.to_s
-	      	redirect_to pets_path
+          redirect_to pets_path
+          flash[:notice] = "You have successfully logged in"
 	    else
           render "new"
+          flash.now[:info] = "You have successfully logged in"
 	      	# @error = "Email and password do not match"
 	    end
   	end
